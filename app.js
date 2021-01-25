c8bd7c9c743ffa77a8ee9ba04a20f3b4db39c68a
D. Strong <derrickmstrong@gmail.com>
2021-01-25 01:17:37 +0000

    Added Computed property for classes

--- app.js
+++ app.js
@@ -5,17 +5,27 @@ const app = Vue.createApp({
       boxA: false,
       boxB: false,
       boxC: false,
+      boxD: false,
     }
   },
 
   methods: {
-    // Change each box (boxA, boxB, boxC) to toggle true/false based on if the bound @click event is activiated (toggles true the first time because initial data property is set to false)
+    // Change each box (boxA, boxB, boxC, boxD) to toggle true/false based on if the bound @click event is activiated (toggles true the first time because initial data property is set to false)
     selectBox(box) {
       return box === 'A' ? this.boxA = !this.boxA 
       : box === 'B' ? this.boxB = !this.boxB
       : box === 'C' ? this.boxC = !this.boxC 
+      : box === 'D' ? this.boxD = !this.boxD
       : null
     }
   },
+
+  computed: {
+    boxDClasses() {
+      return {
+        active: this.boxD,
+      }
+    }
+  }
 
 }).mount('#styling')
